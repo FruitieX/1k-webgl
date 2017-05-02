bd60c59a19d5b4341a7d33c0f9da0d6b2befb7c4
Rasmus Eskola <fruitiex@gmail.com>
2017-05-02 15:47:04 +0300

    make minified output behave identical to development build

--- src/index.js
+++ src/index.js
@@ -41,34 +41,32 @@ m = _ => {
 }
 
 // onload
-l = () => {
-  g = c.getContext('webgl');
+g = c.getContext('webgl');
 
-  P = g.createProgram();
+P = g.createProgram();
 
-  // vertex shader
-  g.shaderSource(S=g.createShader(35633), require('./vertex.glsl')); // g.VERTEX_SHADER = 35633
-  g.compileShader(S);g.attachShader(P,S);
+// vertex shader
+g.shaderSource(S=g.createShader(35633), require('./vertex.glsl')); // g.VERTEX_SHADER = 35633
+g.compileShader(S);g.attachShader(P,S);
 
-  // fragment shader
-  g.shaderSource(S=g.createShader(35632), require('./fragment.glsl')); // g.FRAGMENT_SHADER = 35632
-  g.compileShader(S);g.attachShader(P,S);
+// fragment shader
+g.shaderSource(S=g.createShader(35632), require('./fragment.glsl')); // g.FRAGMENT_SHADER = 35632
+g.compileShader(S);g.attachShader(P,S);
 
-  // Check for any compilation error
-  // TODO: remove in production
-  if (!g.getShaderParameter(S, 35713)) { // g.COMPILE_STATUS = 35713
-      alert(g.getShaderInfoLog(S));
-  }
-
-  g.linkProgram(P);
-  g.useProgram(P);
-
-  // g.ARRAY_BUFFER = 34962
-  g.bindBuffer(34962, g.createBuffer());
-  g.bufferData(34962, new Int8Array([-3,1,1,-3,1,1]),35044); // 35044 = gl.STATIC_DRAW
-  g.enableVertexAttribArray(0);
-  g.vertexAttribPointer(0,2,5120,0,0,0); // g.BYTE = 5120
-
-  // start rendering and music playback
-  r();m();
+// Check for any compilation error
+// TODO: remove in production
+if (!g.getShaderParameter(S, 35713)) { // g.COMPILE_STATUS = 35713
+    alert(g.getShaderInfoLog(S));
 }
+
+g.linkProgram(P);
+g.useProgram(P);
+
+// g.ARRAY_BUFFER = 34962
+g.bindBuffer(34962, g.createBuffer());
+g.bufferData(34962, new Int8Array([-3,1,1,-3,1,1]),35044); // 35044 = gl.STATIC_DRAW
+g.enableVertexAttribArray(0);
+g.vertexAttribPointer(0,2,5120,0,0,0); // g.BYTE = 5120
+
+// start rendering and music playback
+r();m();
