2abc8e3060ed2413e858f67ee6ffd50385e5536a
Rasmus Eskola <fruitiex@gmail.com>
2017-05-03 21:33:37 +0300

    Add regpack to minify script

--- minify.sh
+++ minify.sh
@@ -4,6 +4,7 @@
 #
 # npm install -g mishoo/UglifyJS2\#harmony
 # npm install -g glslmin
+# npm install -g regpack
 
 rm -rf dist
 
@@ -12,16 +13,22 @@ cp mintemplate.html dist/temp/temp1.html
 cd dist
 
 # js src
-uglifyjs ../src/song.js ../src/player.js ../src/index.js | node ../utils/findandreplace.js --template temp/temp1.html --find '{{javascript}}' > temp/temp2.html
+#uglifyjs ../src/song.js ../src/player.js ../src/index.js | node ../utils/findandreplace.js --template temp/temp1.html --find '{{javascript}}' > temp/temp2.html
+# crunch with regpack
+echo "uglifying..."
+uglifyjs ../src/song.js ../src/player.js ../src/index.js > temp/temp1.js
 
 # vertex shader
-glslmin ../src/vertex.glsl | node ../utils/findandreplace.js --template temp/temp2.html --find 'require("./vertex.glsl")' --surround '`' > temp/temp3.html
-#cat ../src/vertex.glsl | node ../utils/findandreplace.js --template temp/temp2.html --find 'require("./vertex.glsl")' --surround '`' > temp/temp3.html
+echo "minifying vertex shader..."
+glslmin ../src/vertex.glsl | node ../utils/findandreplace.js --template temp/temp1.js --find 'require("./vertex.glsl")' --surround '`' > temp/temp2.js
 
 # fragment shader
-glslmin -m ../src/fragment.glsl | node ../utils/findandreplace.js --template temp/temp3.html --find 'require("./fragment.glsl")' --surround '`' > temp/temp4.html
-#cat ../src/fragment.glsl | node ../utils/findandreplace.js --template temp/temp3.html --find 'require("./fragment.glsl")' --surround '`' > temp/temp4.html
+echo "minifying fragment shader..."
+glslmin -m ../src/fragment.glsl | node ../utils/findandreplace.js --template temp/temp2.js --find 'require("./fragment.glsl")' --surround '`' > temp/temp3.js
 
-cp temp/temp4.html index.html
+echo "running regpack..."
+regpack temp/temp3.js | node ../utils/findandreplace.js --template temp/temp1.html --find '{{javascript}}' > temp/temp.html
+
+cp temp/temp.html index.html
 
 echo "wrote index.html ($(cat index.html | wc -c) bytes)"
