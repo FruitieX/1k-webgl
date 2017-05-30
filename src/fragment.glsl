6cebe815f5c2f39374073128827a590819a68448
Rasmus Eskola <fruitiex@gmail.com>
2017-05-30 16:07:05 +0300

    Code golfing

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -39,77 +39,67 @@ void main() {
   //vec2 res = vec2(240, 135);
   //for( float m=0.; m<2.; m++ )   // 2x AA
   //for( float n=0.; n<2.; n++ ) { // 2x AA
-    //vec2 p = (-res.xy + 3.*(gl_FragCoord.xy+o))/res.y;
-    //vec2 p = ;
+  //vec2 p = (-res.xy + 3.*(gl_FragCoord.xy+o))/res.y;
+  //vec2 p = ;
 
-    // camera
-    // ro = ray origin = where the camera is
-    // ta = camera direction (where the camera is looking)
-    // cr = camera rotation
-    //vec3 ro = vec3( -.5+3.5*cos(.1*a), 1.0, .5 + 4.0*sin(.1*a) );
-    vec3 ro = vec3( cos(a), 1., sin(a) );
-    // camera-to-world transformation
-    //mat3 ca = setCamera(ro);
+  // camera
+  // ro = ray origin = where the camera is
+  // ta = camera direction (where the camera is looking)
+  // cr = camera rotation
+  //vec3 ro = vec3( -.5+3.5*cos(.1*a), 1.0, .5 + 4.0*sin(.1*a) );
+  vec3 ro = vec3( cos(a), 1., sin(a) );
+  // camera-to-world transformation
+  //mat3 ca = setCamera(ro);
 
-  	vec3 cw = normalize(-ro);
-  	vec3 cu = cross(cw, vec3(.0, 1., .0));
+	vec3 cw = normalize(-ro);
+	vec3 cu = cross(cw, vec3(.0, 1., .0));
 
-    // ray direction
-    vec3 rd = mat3(
-      cu,
-      cross(cu,cw),
-      cw
-    ) * normalize(
-      vec3(
-        (
-          -vec2(240., 135.) + 2. * (
-            gl_FragCoord.xy //+
-            // pixel coordinates when doing AA
-            //vec2(m,n) / 2. - .5
-          )
-        ) / 135.,
-        2.
-      )
-    );
+  // ray direction
+  vec3 rd = mat3(
+    cu,
+    cross(cu,cw),
+    cw
+  ) * normalize(
+    vec3(
+      (
+        -vec2(240., 135.) + 2. * (
+          gl_FragCoord.xy //+
+          // pixel coordinates when doing AA
+          //vec2(m,n) / 2. - .5
+        )
+      ) / 135.,
+      2.
+    )
+  );
 
-    // render
-    //vec3 col = render( ro, rd );
+  // render
+  //vec3 col = render( ro, rd );
 
-    // castRay(ro, rd)
-    float tmin = 1., mat = -1.;
-    for( float i=.0; i<64.; i++ ) { // 64 = maxIterations
-      cw = ro+rd*tmin;
-      vec2 rayRes = map(cw);
-      //float precis = .000001*tmin;
-      //if( rayRes.x<precis) break; // TODO: nice optimisation, but eats space
-      tmin += rayRes.x;
-      mat = rayRes.y;
-    }
+  // castRay(ro, rd)
+  float tmin = 1., mat = -1.;
+  for( float i=.0; i<64.; i++ ) { // 64 = maxIterations
+    cw = ro+rd*tmin;
+    vec2 rayRes = map(cw);
+    //float precis = .000001*tmin;
+    //if( rayRes.x<precis) break; // TODO: nice optimisation, but eats space
+    tmin += rayRes.x;
+    mat = rayRes.y;
+  }
 
-    //if( tmin>30. ) mat=-1.; // 30. = tmax
-    // end
+  //if( tmin>30. ) mat=-1.; // 30. = tmax
+  // end
 
-    // vec3 nor = calcNormal(ro + tmin*rd);
-    // calcNormal(pos)
-    vec2 e = vec2(.01,-.01); // (1, -1) * .5773*.0005
-    // normal
-    cu = normalize(
-      e.xyy*map( cw+ e.xyy ).x +
-      e.yyx*map( cw+ e.yyx ).x +
-      e.yxy*map( cw+ e.yxy ).x +
-      e.xxx*map( cw+ e.xxx ).x
-    );
-    // end
+  vec2 e = vec2(.01,-.01); // (1, -1) * .5773*.0005
+  cu = normalize(
+    e.xyy*map( cw + e.xyy ).x +
+    e.yyx*map( cw + e.yyx ).x +
+    e.yxy*map( cw + e.yxy ).x +
+    e.xxx*map( cw + e.xxx ).x
+  );
 
-    tot +=
-      .5 * sin(vec3(.05,.08,.1) * mat) + //* vec3(dot(cu, vec3(1.))) +
-      .5 * vec3(reflect( rd, cu ).y);
-
-  	// gamma
-    //col = pow( col, vec3(.7) );
-
-    //tot /= 4.; // AA * AA
-  //}
+  tot +=
+    .5 * sin(vec3(.05,.08,.1) * mat) + //* vec3(dot(cu, vec3(1.))) +
+    .5 * vec3(reflect( rd, cu ).y);
 
   gl_FragColor = vec4( tot, 1. );
 }
