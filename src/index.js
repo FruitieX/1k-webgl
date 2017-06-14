49d56f0c4d278bbb3abdcff3d802020bdcccfcab
Rasmus Eskola <fruitiex@gmail.com>
2017-06-14 15:59:40 +0300

    more comments

--- src/index.js
+++ src/index.js
@@ -8,59 +8,59 @@ a.connect(f.destination);
 
 // sequencer thing
 s=(notes,octave,rate,len) =>
-  31 & t * Math.pow(2, notes[(t>>rate)%len] / 12 - octave)
-
-// music debug
-//t= 1744100
-//t= 3744100
-//t= 4744100
+  31 & t * Math.pow(2, notes[(t>>rate) % len] / 12 - octave)
 
 // music
 X = a.onaudioprocess = e =>
-  //for(L = e.outputBuffer.getChannelData(i=0);i++<512;t++) {
-  //e.outputBuffer.getChannelData(i=0);i++<512;t++) {
   e.outputBuffer.getChannelData(i=0).map(_ =>
     e.outputBuffer.getChannelData(0)[i++] =
     (
       (
         // kick drum
-        (
-          (
-            K = 1e4 / (
-              t & 16382
-              /* * (
-                (t>>14) % 16 - 15 ? 1 : .75
-              )*/
-            )
-          ) & 1
-        ) * 30
+        ((
+          // envelope
+          K = 1e4 / (
+            t & 16382
+            // nice pattern variation if it fits
+            /* * (
+              (t>>14) % 16 - 15 ? 1 : .75
+            )*/
+          )
+        ) & 1) * 30
 
         // bass
-        + s('7050',4,17,4) / K
+        + s(
+          '7050', // melody
+          4,      // octave
+          17,     // rate
+          4       // melody length
+        ) / K
       )
 
       // turn off above instruments after a while
       * !(t>>22)
 
       // hihat TODO improve/golf envelope
-      + (t % 100 * t & 128) * Math.min(.2, (1e1 / ((t>>3) % 512))) * !!(t>>19)
+      + (t % 100 * t & 128) * Math.min(.2, (1e1 / ((t>>3) % 512)))
+
+      // enable hihat after t>>19
+      * !!(t>>19)
 
       // arpeggio
-      + (s((t>>17)%2 ? '027' : '037',1,13-(3*(t>>20)%12),4)) / K * !!(t>>20)
+      + s(
+        (t>>17) % 2 ? '027' : '037',  // melody
+        1,                            // octave
+        13 - 3 * (t>>20) % 12,        // rate
+        4                             // melody length
+      ) / K
 
+      // enable arpeggio after t>>20
+      * !!(t>>20)
     ) * (
       // fade out
       X = Math.min(1, Math.max(1e-9, 1e1-t++/5e5))
     ) / 200
   );
-
-    //L[i] *= X / 200;
-    //if (L[i] > 1) console.log('clipping');
-
-    // limit volume while testing
-    //L[i] = Math.max(-0.5, Math.min(0.5, L[i]));
-  //}
-
 
 // gfx
 g = c.getContext`webgl`;
@@ -68,9 +68,10 @@ P = g.createProgram();
 
 // NOTE: 2nd argument to drawArrays used to be 0, but undefined works
 r = time => g.drawArrays(g.TRIANGLE_FAN,
-  // Send resolution and time to shader
+  // x-res, y-res, time (s), fade out
   g.uniform4f(g.getUniformLocation(P, 'a'), c.width, c.height, time / 1e3, X),
   3,
+  // 1, 0, kick envelope, unused
   g.uniform4f(g.getUniformLocation(P, 'b'), 1, 0, .2/K, requestAnimationFrame(r))
 );
 
