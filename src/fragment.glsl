a9a04e3de8f74390a69950a93c89c77f280b894e
Rasmus Eskola <fruitiex@gmail.com>
2017-06-16 23:00:45 +0300

    golf (+27)

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -12,22 +12,22 @@ uniform vec4 b;
 
 void main() {
   // ray marcher temp result
-	vec3 e = b.yyy,
-  cu = b.xxx,
+	vec4 e = b.yyyy,
+  cu = b.xxxx,
 
   // ray direction
-  rd = vec3(2. * gl_FragCoord.xy - a.xy, a.y) / a.y - cu;
+  rd = vec4(2. * gl_FragCoord.xy - a.xy, a.xy) / a.y - cu;
 
   // ray marcher
-  for( float i=1e0; i<1e1; i++ ) {
+  for( float i=1.; i<1e1; i++ ) {
     if(e.x < 1e0) // results in trippy background
       cu += (e =
 				// x = distance to sdf
 				// y = material color
 				// z = unused
-        vec3(
+        vec4(
           // sphere
-          length(b.xxz+rd*cu) - 1e0 +
+          length(b.xxyy+rd*cu) - 1e0 +
 
           // plasma
           .1 * sin(b.z + 1e1 * rd.x*cu.x + a.z)
