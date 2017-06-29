79212e5f4394e3b85ca771b4a292070a010ae3fa
Rasmus Eskola <fruitiex@gmail.com>
2017-06-29 14:29:22 +0300

    Trailing whitespace

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -26,7 +26,7 @@ void main() {
 
   gl_FragColor = vec4(
     // material color
-    sin(sin(sin(e.y - a.xyz) * 2. + b.y) / 1e1 + e.y) * a.w, 
+    sin(sin(sin(e.y - a.xyz) * 2. + b.y) / 1e1 + e.y) * a.w,
 
 		1e0
   );
