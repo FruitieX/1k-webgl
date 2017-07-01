61a3e57388bc5d23af84a43fca8e345459afbb7d
Rasmus Eskola <fruitiex@gmail.com>
2017-07-01 18:45:23 +0300

    Golf + color improvements

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -25,7 +25,7 @@ void main() {
 
   gl_FragColor = vec4(
     // material color
-    sin(sin(sin(e.z - a.xyz) * 2. + b.y) / 1e1 + e.z) * a.w,
+    sin(sin(sin(e.z - a.xyz) * a.z + b.y) / 1e1 + e.z) * a.w,
 
 		1e0
   );
